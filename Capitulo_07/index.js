42274da206b81947c26c0732eb77d85c0b46290e
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-24 20:01:18 -0300

    Entender variáveis não inicializadas

--- Capitulo_07/index.js
+++ Capitulo_07/index.js
@@ -1,2 +1,9 @@
-var myFirstName = 'Edivaldo';
-var myLastName = 'Junior';
+// Altere apenas o código abaixo desta linha
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Altere apenas o código acima desta linha
+a = a + 1;
+b = b + 5;
+c = c + " String!";
