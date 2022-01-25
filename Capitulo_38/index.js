15a70c02a7e2a4af1bd1e8242d532bc2f0ba994c
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-25 20:07:13 -0300

    Preencher espaços em branco

---
+++ Capitulo_38/index.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Altere apenas o código abaixo desta linha
+const wordBlanks = "On a sunny day I was " + myAdverb + " with " + myAdjective + " velocity " + myVerb + " great " + myNoun + " behind me."; // Altere esta linha
+// Altere apenas o código acima desta linha
