f25a7fe1bbb10cd2509d23bae6e43f4b80753e46
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-25 14:29:19 -0300

    Entender a imutabilidade das strings

---
+++ Capitulo_34/index.js
@@ -0,0 +1,7 @@
+// Configuração
+let myStr = "Jello World";
+
+// Altere apenas o código abaixo desta linha
+myStr = "Hello World"
+myStr[0]; // Altere esta linha
+// Altere apenas o código acima desta linha
