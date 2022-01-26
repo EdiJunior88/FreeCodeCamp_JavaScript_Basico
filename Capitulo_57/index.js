e3cdcf885ba2b52833e93f00029a80132b1bf73c
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-26 19:32:49 -0300

    Ficar na linha

---
+++ Capitulo_57/index.js
@@ -0,0 +1,15 @@
+function nextInLine(arr, item) {
+  // Altere apenas o código abaixo desta linha
+  arr.push(item);
+  const removed = arr.shift();
+  return removed;
+  // Altere apenas o código acima desta linha
+}
+
+// Configuração
+const testArr = [1, 2, 3, 4, 5];
+
+// Exibir o código
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
