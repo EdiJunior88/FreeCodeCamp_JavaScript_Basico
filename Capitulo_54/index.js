ca679c96ad4efc8edfc37ab2b8817d3d3560a9df
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-26 19:13:28 -0300

    Diferenciar escopo global e local em funções

---
+++ Capitulo_54/index.js
@@ -0,0 +1,11 @@
+// Configuração
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Altere apenas o código abaixo desta linha
+  const outerWear = "sweater";
+  // Altere apenas o código acima desta linha
+  return outerWear;
+}
+
+myOutfit();
