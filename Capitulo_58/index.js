07d60f8f4c8e016d28d44cf213f44a554b82f9ee
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-26 19:35:34 -0300

    Entender valores booleanos

---
+++ Capitulo_58/index.js
@@ -0,0 +1,7 @@
+function welcomeToBooleans() {
+  // Altere apenas o código abaixo desta linha
+  console.log('Botão ativado')
+  return true; // Altere esta linha
+
+  // Altere apenas o código acima desta linha
+}
