2d3cbc3c4d9474b5b8a5e912eeb50c3df16ad55d
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-28 20:01:22 -0300

    Jogar golfe de código

---
+++ Capitulo_75/index.js
@@ -0,0 +1,23 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Altere apenas o código abaixo desta linha
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+  // Altere apenas o código acima desta linha
+}
+
+golfScore(5, 4);
