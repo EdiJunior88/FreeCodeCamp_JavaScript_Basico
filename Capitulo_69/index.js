c2006e04e045c6c2e974ad8be365126b1c5f065a
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-28 19:16:25 -0300

    Comparar com o operador lógico AND

---
+++ Capitulo_69/index.js
@@ -0,0 +1,12 @@
+function testLogicalAnd(val) {
+  // Altere apenas o código abaixo desta linha
+
+  if (val > 24 && val < 51) {
+    return "Yes";
+  }
+
+  // Altere apenas o código acima desta linha
+  return "No";
+}
+
+testLogicalAnd(10);
