c8ad38b994bf7b514428b6435405c122f35d8405
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-29 20:14:50 -0300

    Criar objetos JavaScript

---
+++ Capitulo_83/index.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Altere apenas o código abaixo desta linha
+  
+  "name": "Kako",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["kiko", "totó"]
+
+  // Altere apenas o código acima desta linha
+};
