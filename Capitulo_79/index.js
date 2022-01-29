4a6ca78d21f290ad7dc6d66b70a6f5c0242fe0c0
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-28 21:59:56 -0300

    Substituir cadeias de if else por switch

---
+++ Capitulo_79/index.js
@@ -0,0 +1,28 @@
+function chainToSwitch(val) {
+  let answer = "";
+  // Altere apenas o código abaixo desta linha
+
+  switch (val) {
+    case "bob":
+      return "Marley"
+      break;
+    case 1:
+      return "There is no #1";
+      break;
+    case 7:
+      return "Ate Nine";
+      break;
+    case 42:
+      return "The Answer";
+      break;
+    case 99:
+      return "Missed me by this much!";
+      break;
+    default:
+      return "";
+  }
+  // Altere apenas o código acima desta linha
+  return answer;
+}
+
+chainToSwitch(7);
