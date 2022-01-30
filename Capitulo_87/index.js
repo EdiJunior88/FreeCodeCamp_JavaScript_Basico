05175c4e89fd6bc40160c51ce8dcea627f6c545b
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-29 21:50:47 -0300

    Atualizar propriedades do objeto

---
+++ Capitulo_87/index.js
@@ -0,0 +1,11 @@
+// Configuração
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder";
+
+// Altere apenas o código abaixo desta linha
