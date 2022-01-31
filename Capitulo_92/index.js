0cf632ffa59e978b151994c3e41a1e9ed942ffe2
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-31 19:54:51 -0300

    Manipular objetos complexos

---
+++ Capitulo_92/index.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+    "artist": "Michael Jackson",
+    "title": "Billie Jean",
+    "release_year": 1983,
+    "formats": [
+      "VHS",
+      "LP"
+    ]
+  }
+];
