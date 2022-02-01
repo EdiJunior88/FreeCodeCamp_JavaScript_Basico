db1f68075b743c6c76de1a841fc710c6727e3805
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-01 10:34:21 -0300

    Gerar frações aleatórias com JavaScript

---
+++ Capitulo_105/index.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+  // Altere apenas o código abaixo desta linha
+  
+  var result = 0;
+  while (result === 0) {
+    result = Math.random();
+  }
+
+  return result;
+  // Altere apenas o código acima desta linha
+}
