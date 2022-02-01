1a22d53a107f0d4e8366a0ec14a42093b04503b1
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-01 10:36:47 -0300

    Gerar números inteiros aleatórios com JavaScript

---
+++ Capitulo_106/index.js
@@ -0,0 +1,8 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Altere apenas o código abaixo desta linha
+  
+  return Math.floor(Math.random() * 10);
+}
