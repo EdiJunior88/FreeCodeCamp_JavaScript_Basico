f2d5ba941d1c3de5263c3d5c85bff8f2a2a8a559
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-31 22:42:19 -0300

    Iterar através de um array com laço for

---
+++ Capitulo_100/index.js
@@ -0,0 +1,8 @@
+// Configuração
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+// Altere apenas o código abaixo desta linha
+for(let i = 0; i < myArr.length; i++) {
+  console.log(total += myArr[i]);
+}
