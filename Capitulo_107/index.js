ddf1770b57278fc23cb5c9908a6379966a911e80
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-01 10:39:09 -0300

    Gerar números inteiros aleatórios dentro de um intervalo

---
+++ Capitulo_107/index.js
@@ -0,0 +1,8 @@
+function randomRange(myMin, myMax) {
+
+  // Altere apenas o código abaixo desta linha
+
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+
+  // Altere apenas o código acima desta linha
+}
